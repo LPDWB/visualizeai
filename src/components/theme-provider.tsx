1b6d10edee720fed526c9f3d8e3cf98e5f296733
LPDWB <deleterwb@gmail.com>
2025-05-26 22:36:53 +0300

    Fix: Replaced invalid next-themes import with local type interface

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,8 +2,14 @@
 
 import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+interface CustomThemeProviderProps {
+  children: React.ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+}
+
+export function ThemeProvider({ children, ...props }: CustomThemeProviderProps) {
+  return <NextThemesProvider {...(props as any)}>{children}</NextThemesProvider>;
 } 
