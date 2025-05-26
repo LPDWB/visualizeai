974bb5bf642f57e5bd2c4184ebbf142c9f349fbf
LPDWB <deleterwb@gmail.com>
2025-05-26 17:46:19 +0300

    fix: simplify data store implementation

--- src/store/useDataStore.ts
+++ src/store/useDataStore.ts
@@ -1,14 +1,11 @@
 import { create } from 'zustand';
-import { StateCreator } from 'zustand';
 
 interface DataStore {
   parsedData: any[];
   setParsedData: (data: any[]) => void;
 }
 
-const useDataStore = create<DataStore>((set: StateCreator<DataStore>['set']) => ({
+export const useDataStore = create<DataStore>((set) => ({
   parsedData: [],
-  setParsedData: (data: any[]) => set({ parsedData: data }),
-}));
-
-export { useDataStore }; 
+  setParsedData: (data) => set({ parsedData: data }),
+})); 
